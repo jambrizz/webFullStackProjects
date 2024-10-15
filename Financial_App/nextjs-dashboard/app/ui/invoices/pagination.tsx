18e9d9aff05e0fa3db8dfe4982711d56fd7d2c41
jambrizz <ambrizjovani@gmail.com>
2024-10-15 00:53:07 -0700

    uncommented parts of code in pagination.tsx to clear failure to deploy on vercel

--- Financial_App/nextjs-dashboard/app/ui/invoices/pagination.tsx
+++ Financial_App/nextjs-dashboard/app/ui/invoices/pagination.tsx
@@ -3,7 +3,7 @@
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
-import { generatePagination } from '@/app/lib/utils';
+//import { generatePagination } from '@/app/lib/utils';
 import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
@@ -24,7 +24,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
     <>
       {/*  NOTE: Uncomment this code in Chapter 11 */}
 
-      {/* <div className="inline-flex">
+      { <div className="inline-flex">
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
@@ -57,7 +57,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
           href={createPageURL(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div> */}
+      </div> }
     </>
   );
 }
